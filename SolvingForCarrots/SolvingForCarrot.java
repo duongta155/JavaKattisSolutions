8ce0ec9f727459edfb9663b5161e88f2f5ff3a3a
duongta155 <dta1@villanova.edu>
2018-11-04 00:05:22 -0400

    carrots solution

---
+++ SolvingForCarrots/SolvingForCarrot.java
@@ -0,0 +1,21 @@
+//*********************************************************************************************
+// SolvingForCarrots.java                                                Author: Duong Ta  
+//
+// Solving the for carrots problem
+//*********************************************************************************************
+
+import java.util.*;
+import java.io.*;
+
+public class SolvingForCarrots 
+{
+  public static void main(String[] args) throws IOException
+  {
+    Scanner scan = new Scanner(System.in);
+    //Scanner scan = new Scanner(new File("test.txt"));
+    
+    int x = scan.nextInt();
+    int y = scan.nextInt();
+    System.out.println(y);
+  }
+}
